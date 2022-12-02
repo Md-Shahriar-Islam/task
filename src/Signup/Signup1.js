eb8956c3edbc78c78fcb12d0a6a7a9cc821b2812
Md Shahriar Islam <mdshahriarislam23@gmail.com>
2022-12-02 16:48:11 +0600

    signup page 1 complete

--- src/Signup/Signup1.js
+++ src/Signup/Signup1.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Background from '../Background/Background';
+import './Signup1.css'
 
 const Signup1 = () => {
     return (
-        <div>
+        <div className='page-display'>
             <div>
                 <Background></Background>
             </div>
-            <div>
+            <div className='box shadow-2xl'>
+
+                <div className='box-inside'>
+                    <h1 className='text-center head-text'>SignUp Form</h1>
+                    <form>
+                        <input className='input-field input-field1' type='text' placeholder='Write First Name'></input><br></br>
+                        <input className='input-field input-field2' type='text' placeholder='Write Last Name'></input><br></br>
+                        <input className='btn btn-info button' type='submit' value='Next Step'></input>
+                    </form>
+                </div>
 
             </div>
+
         </div>
     );
 };
